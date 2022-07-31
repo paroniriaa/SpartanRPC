069999075bc742590c3db16aa0bf4ad4cf14eeef
Jiahongzhan <13570113219@163.com>
2022-07-30 18:31:39 -0700

    nux and windows test

--- test/HTTP_test.go
+++ test/HTTP_test.go
@@ -10,20 +10,43 @@ import (
 )
 
 func TestXDial(t *testing.T) {
-	if runtime.GOOS == "window" {
-		ch := make(chan struct{})
-		addr := "/tmp/RPC.sock"
-		go func() {
-			_ = os.Remove(addr)
-			l, err := net.Listen("winsock", addr)
-			if err != nil {
-				t.Fatal("failed to listen windows socket")
-			}
-			ch <- struct{}{}
-			server.AcceptConnection(l)
-		}()
-		<-ch
-		_, err := client.XMakeDial("widnows@" + addr)
-		_assert(err == nil, "failed to connect windows socket")
-	}
+
+	t.Run("windows", func(t *testing.T) {
+		if runtime.GOOS == "window" {
+			ch := make(chan struct{})
+			addr := "/tmp/RPC.sock"
+			go func() {
+				_ = os.Remove(addr)
+				l, err := net.Listen("winsock", addr)
+				if err != nil {
+					t.Fatal("failed to listen windows socket")
+				}
+				ch <- struct{}{}
+				server.AcceptConnection(l)
+			}()
+			<-ch
+			_, err := client.XMakeDial("widnows@" + addr)
+			_assert(err == nil, "failed to connect windows socket")
+		}
+	})
+
+
+	t.Run("linux", func(t *testing.T) {
+		if runtime.GOOS == "linux" {
+			ch := make(chan struct{})
+			addr := "/tmp/RPC.sock"
+			go func() {
+				_ = os.Remove(addr)
+				l, err := net.Listen("unix", addr)
+				if err != nil {
+					t.Fatal("failed to listen unix socket")
+				}
+				ch <- struct{}{}
+				server.AcceptConnection(l)
+			}()
+			<-ch
+			_, err := client.XMakeDial("linux@" + addr)
+			_assert(err == nil, "failed to connect unix socket")
+		}
+	})
 }
