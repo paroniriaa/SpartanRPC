e20e96646a4ea4ede25958563b44020725f00113
paroniriaa <44891450+paroniriaa@users.noreply.github.com>
2022-11-21 00:02:50 -0800

    Update coder_test.go
    added sub task test for test clarification

--- test/coder_test.go
+++ test/coder_test.go
@@ -6,14 +6,16 @@ import (
 	"testing"
 )
 
-func TestCoder(test *testing.T) {
-	test.Helper()
-	TestNewCoderFuncMap := make(map[coder.CoderType]coder.CoderInitializer)
-	TestNewCoderFuncMap[coder.Json] = coder.NewJsonCoder
-	if reflect.TypeOf(coder.CoderInitializerMap) != reflect.TypeOf(TestNewCoderFuncMap) {
-		test.Errorf("CoderInitializerMap expected be in type of %s, but got %s", reflect.TypeOf(TestNewCoderFuncMap), reflect.TypeOf(coder.CoderInitializerMap))
-	}
-	if reflect.TypeOf(coder.CoderInitializerMap[coder.Json]) != reflect.TypeOf(TestNewCoderFuncMap[coder.Json]) {
-		test.Errorf("CoderInitializerMap[Json] expected be in type of %s, but got %s", reflect.TypeOf(coder.CoderInitializerMap[coder.Json]), reflect.TypeOf(TestNewCoderFuncMap[coder.Json]))
-	}
+func TestCoder(t *testing.T) {
+	t.Helper()
+	t.Run("CoderInitialization", func(t *testing.T) {
+		TestNewCoderFuncMap := make(map[coder.CoderType]coder.CoderInitializer)
+		TestNewCoderFuncMap[coder.Json] = coder.NewJsonCoder
+		if reflect.TypeOf(coder.CoderInitializerMap) != reflect.TypeOf(TestNewCoderFuncMap) {
+			t.Errorf("CoderInitializerMap expected be in type of %s, but got %s", reflect.TypeOf(TestNewCoderFuncMap), reflect.TypeOf(coder.CoderInitializerMap))
+		}
+		if reflect.TypeOf(coder.CoderInitializerMap[coder.Json]) != reflect.TypeOf(TestNewCoderFuncMap[coder.Json]) {
+			t.Errorf("CoderInitializerMap[Json] expected be in type of %s, but got %s", reflect.TypeOf(coder.CoderInitializerMap[coder.Json]), reflect.TypeOf(TestNewCoderFuncMap[coder.Json]))
+		}
+	})
 }
