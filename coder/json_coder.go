306d2defe6800c18a05affb17e4334ec640e7341
paroniriaa <44891450+paroniriaa@users.noreply.github.com>
2022-12-01 03:42:45 -0800

    Update json_coder.go
    improved readability

--- coder/json_coder.go
+++ coder/json_coder.go
@@ -18,40 +18,46 @@ var _ Coder = (*JsonCoder)(nil)
 
 func NewJsonCoder(connection io.ReadWriteCloser) Coder {
 	buffer := bufio.NewWriter(connection)
-	return &JsonCoder{
+	newJsonCoder := &JsonCoder{
 		connection: connection,
 		buffer:     buffer,
 		encoder:    json.NewEncoder(buffer),
 		decoder:    json.NewDecoder(connection),
 	}
+	log.Printf("RPC JsonCoder -> NewJsonCoder: RPC JsonCoder %p created with field -> %+v", newJsonCoder, newJsonCoder)
+	return newJsonCoder
 }
 
-func (coder *JsonCoder) DecodeMessageHeader(header *MessageHeader) error {
-	return coder.decoder.Decode(header)
+func (jsonCoder *JsonCoder) DecodeMessageHeader(header *MessageHeader) error {
+	log.Printf("RPC JsonCoder -> DecodeMessageHeader: RPC JsonCoder %p deconding message header...", jsonCoder)
+	return jsonCoder.decoder.Decode(header)
 }
 
-func (coder *JsonCoder) DecodeMessageBody(body interface{}) error {
-	return coder.decoder.Decode(body)
+func (jsonCoder *JsonCoder) DecodeMessageBody(body interface{}) error {
+	log.Printf("RPC JsonCoder -> DecodeMessageBody: RPC JsonCoder %p deconding message body...", jsonCoder)
+	return jsonCoder.decoder.Decode(body)
 }
 
-func (coder *JsonCoder) EncodeMessageHeaderAndBody(header *MessageHeader, body interface{}) (error error) {
-	if error = coder.encoder.Encode(header); error != nil {
+func (jsonCoder *JsonCoder) EncodeMessageHeaderAndBody(header *MessageHeader, body interface{}) (error error) {
+	log.Printf("RPC JsonCoder -> EncodeMessageHeaderAndBody: RPC JsonCoder %p encoding message header and body...", jsonCoder)
+	if error = jsonCoder.encoder.Encode(header); error != nil {
 		log.Println("RPC Json Coder -> EncodeMessageHeaderAndBody Error: error when encoding header:", error)
 		return error
 	}
-	if error = coder.encoder.Encode(body); error != nil {
+	if error = jsonCoder.encoder.Encode(body); error != nil {
 		log.Println("RPC Json Coder -> EncodeMessageHeaderAndBody Error: error when encoding body:", error)
 		return error
 	}
 	defer func() {
-		_ = coder.buffer.Flush()
+		_ = jsonCoder.buffer.Flush()
 		if error != nil {
-			_ = coder.Close()
+			_ = jsonCoder.Close()
 		}
 	}()
 	return error
 }
 
-func (coder *JsonCoder) Close() error {
-	return coder.connection.Close()
+func (jsonCoder *JsonCoder) Close() error {
+	log.Printf("RPC JsonCoder -> Close: RPC JsonCoder %p is closing...", jsonCoder)
+	return jsonCoder.connection.Close()
 }
